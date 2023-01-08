8136fae9308ec3c304d403196379f211dd4a15ea
Taha Ansari <taha.m.ansari@gmail.com>
2023-01-08 18:52:37 +0530

    life cycle methods

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import "./App.css";
 import User from "./User";
 import ClassUser from "./ClassUser";
 import Login from "./Login";
+// import React from "react";
+
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("taha");
   return (
     <div className="App">
       <div>
-        {/* <ClassUser name="Taha Ansari" age="31" /> */}
-        <Login></Login>
+        {/* <h1>Hello, {t his.state.name}</h1>
+          <button
+            onClick={() => {
+              this.setState({ name: "Taha Mehmood Ansari" });
+            }}
+          >
+            change name
+          </button> */}
+        <ClassUser name={name} />
+        <button
+          onClick={() => {
+            setName("Taha Mehmood Ansari");
+          }}
+        >
+          change state
+        </button>
       </div>
     </div>
   );
--- src/ClassUser.js
+++ src/ClassUser.js
@@ -4,20 +4,33 @@ class ClassUser extends Component {
   constructor() {
     super();
     this.state = {
-      count: 0,
+      name: "taha",
+      age: 31,
     };
   }
-  udpateCount() {
-    console.log("udpate count called");
-    this.setState({
-      count: this.state.count + 1,
-    });
+  componentDidMount() {
+    // called after all the html gets loaded
+    console.log("component did mount");
   }
+  componentDidUpdate(preProps, preState, snapshot) {
+    // called when any component get update
+    console.log("component did update", preState);
+    this.setState({ age: this.state.age + 1 });
+  }
+  // udpateCount() {
+  //   console.log("udpate count called");
+  //   this.setState({
+  //     count: this.state.count + 1,
+  //   });
+  // }
   render() {
-    console.log(this.props);
+    console.warn("render method called", this.props);
     return (
       <>
-        <h1>{this.state.count}</h1>
+        <h1>User Component, {this.state.name}</h1>
+        <p>{this.state.age}</p>
+        <button onClick={() => this.setState({ age: this.state.age + 1 })}>Increment by 1</button>
+        {/* <h1>{this.state.count}</h1>
         <h2>{this.props.name}</h2>
         <button
           onClick={() => {
@@ -25,7 +38,7 @@ class ClassUser extends Component {
           }}
         >
           udpate count
-        </button>
+        </button> */}
       </>
     );
   }
