8d5bcda5c1a2436b54e8ce9b8f7187972640d33d
Taha Ansari <taha.m.ansari@gmail.com>
2023-01-11 12:11:37 +0530

    useref

--- src/App.js
+++ src/App.js
@@ -1,131 +1,19 @@
-import React, { PureComponent, useState } from "react";
-import MyUseEffect from "./MyUseEffect";
+import React, { Component, useState, createRef } from "react";
 
-import { Button, Alert } from "react-bootstrap";
-
-import { Table } from "react-bootstrap";
-import User from "./User";
-import { render } from "@testing-library/react";
-
-class App extends React.PureComponent {
-  // const [counter, setCounter] = useState(10);
-  // const [data, setData] = useState(100);
-  // function showAlert(data) {
-  //   alert(data);
-  // }
-
+class App extends Component {
   constructor() {
     super();
-    console.log("app js constructor");
-
-    this.state = {
-      count: 0,
-    };
+    console.log("constructor function in app.js component");
+    this.inputRef = createRef();
   }
-
+  componentDidMount() {
+    console.log((this.inputRef.current.value = "10000"));
+  }
   render() {
-    console.log("render method called");
-    const users = [
-      {
-        name: "taha",
-        age: 21,
-        gender: "male",
-        address: [
-          { state: "maharashtra", city: "mumbai" },
-          { state: "delhi", city: "chandni chowk" },
-          { state: "punjab", city: "amritsar" },
-        ],
-      },
-      {
-        name: "adil",
-        age: 31,
-        gender: "male",
-        address: [
-          { state: "delhi", city: "chandni chowk" },
-          { state: "maharashtra", city: "mumbai" },
-          { state: "punjab", city: "amritsar" },
-        ],
-      },
-      {
-        name: "maaz",
-        age: 35,
-        gender: "male",
-        address: [
-          { state: "punjab", city: "amritsar" },
-          { state: "delhi", city: "chandni chowk" },
-          { state: "maharashtra", city: "mumbai" },
-        ],
-      },
-    ];
     return (
       <div className="App" style={{ textAlign: "center" }}>
-        <h1>App.js Component</h1>
-        <h2>Counter:{this.state.count}</h2>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>click me</button>
-        {/* <Button onClick={() => alert("hello")}>Click Me</Button>
-        <MyUseEffect data={data} counter={counter}></MyUseEffect>
-        <button
-          onClick={() => {
-            setCounter(counter + 1);
-          }} 
-        >
-          Update Count
-        </button>
-        <button
-          onClick={() => {
-            setData(data + 1);
-          }}
-        >
-          Update Data
-        </button> */}
-
-        {/* {users.map((user, index) => (
-          <div key={index}>
-            <User showAlert={showAlert} data={user}></User>
-          </div>
-        ))} */}
-
-        {/* <Table striped bordered hover variant="dark">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Age</th>
-              <th>Gender</th>
-              <th>Address</th>
-            </tr>
-          </thead>
-          <tbody>
-            {arr.map((item, index) => (
-              <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{item.name}</td>
-                <td>{item.age}</td>
-                <td>{item.gender}</td>
-                <td>
-                  <Table striped bordered hover variant="light">
-                    <thead>
-                      <tr>
-                        <th>#</th>
-                        <th>State</th>
-                        <th>City</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {item.address.map((address, index) => (
-                        <tr key={index}>
-                          <td>{index + 1}</td>
-                          <td>{address.state}</td>
-                          <td>{address.city}</td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </Table>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table> */}
+        <h1>Ref in React</h1>
+        <input type="text" defaultValue="10" ref={this.inputRef} />
       </div>
     );
   }
